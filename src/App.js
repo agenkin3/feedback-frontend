34b791d442d94907418a701b19d16607e510f2f0
Alice Genkin <agenkin1@gmail.com>
2021-08-25 09:09:01 -0400

    install packages and import files

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 class App extends React.Component {
 
 componentDidMount() {
-   fetch('http://127.0.0.1:3000/api/v1/students')
+   fetch('http://127.0.0.1:3000/api/v1/students/1/evaluations')
     .then(response => response.json())
     .then (data => console.log(data))
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux'
 
 import App from './App';
 
@@ -8,3 +11,9 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//set up store
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+);
