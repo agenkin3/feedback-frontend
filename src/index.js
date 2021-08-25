5af6efb99ded8e46f6a4d41cc91d6c44679db6a2
Alice Genkin <agenkin1@gmail.com>
2021-08-25 13:33:20 -0400

    set up index.js file and created store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux'
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_|| compose;
+
+
+
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk))
+
+ReactDOM.render(
+<Provider store=(store) >
+  <App/>
+  </Provider>
+
+ document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
