560b527b904547da770cbfbe6857262750d34fe9
Alice Genkin <agenkin1@gmail.com>
2021-09-03 14:59:50 -0400

    fix delete eval

--- src/actions/deleteEvaluation.js
+++ src/actions/deleteEvaluation.js
@@ -1,10 +1,9 @@
 export const deleteEvaluation = (evaluationId, studentId) => {
-
   return (dispatch) => {
     return fetch(`http://localhost:3000/api/v1/students/${studentId}/evaluations/${evaluationId}`, {
       method: 'DELETE'
     })
-    .then(response => response.json())
-    .then(student => dispatch({type: 'DELETE_EVALUATION', payload: student}))
+    // .then(response => response.json())
+    .then(student => dispatch({type: 'DELETE_EVALUATION', payload: {evaluationId, studentId}}))
   }
 }
--- src/reducers/studentReducer.js
+++ src/reducers/studentReducer.js
@@ -15,8 +15,11 @@ export default function studentReducer(state = {students: []}, action) {
           return {...state, students: students}
         case 'DELETE_EVALUATION':
           let studentsTwo = state.students.map(student => {
-            if (student.id === action.payload.id) {
-              return action.payload
+            if (student.id === action.payload.studentId) {
+              student.evaluations = student.evaluations.filter(evaluation => {
+                return evaluation.id !== action.payload.evaluationId
+              }) 
+              return student
             } else {
               return student
             }
